41080022a532f44718e9276d48a7387001d31321
Oleh Pipenko <pguruop@gmail.com>
2022-07-14 10:01:52 +0200

    commandline uses only 4 arithmetic operators

--- calculate.py
+++ calculate.py
@@ -1,11 +1,16 @@
 import sys
 value = ""
+operations = ["+", "-", "*", "/"]
 if len(sys.argv) == 4:
    try:
       for arg in range(1, len(sys.argv)): 
+         if arg == 2 and sys.argv[arg] not in operations:
+            print("invalid arithmetic operator")
+            value = ""
+            break
          value += sys.argv[arg]
       print(eval(value))
    except:
-      print("Please use numbers and arithmetic operators")
+      print("Please use numbers and acceptable arithmetic operators")
 else:
    print("Number of parameters for calculation not enough. Needed 4 but was entered {}. 1 - 3 parameters for calculation".format(len(sys.argv)))
